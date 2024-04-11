e3b269f853f48df0d72af57c5c2751cd18e44396
mFnana <79454483+mFnana@users.noreply.github.com>
2024-04-12 00:21:24 +0700

    Update main.py
    add new future, memorize

--- python/main.py
+++ python/main.py
@@ -8,6 +8,21 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.graphics import Color, Rectangle
 from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
+import sqlite3
+class SqlDb:
+	def __init__(self, database, table):
+		self.dat = database
+		self.tab = table
+	def get(self):
+		self.refresh()
+		return list(self.cur.execute(f"SELECT * FROM {self.tab}").fetchall()[0])
+	def refresh(self):
+		self.cnn = sqlite3.connect(self.dat)
+		self.cur = self.cnn.cursor()
+	def update(self, dtnm, dlis):
+		self.refresh()
+		self.cur.execute(f"UPDATE {self.tab} SET 	{dtnm[0]} = {dlis[0]}, {dtnm[1]} = {dlis[1]} WHERE rowid = 1")
+		self.cnn.commit()
 class B1(Button):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
@@ -29,9 +44,10 @@ class L1(Label):
 		self.rect.pos = self.pos
 class main(App):
 	def build(self):
+		self.sql = SqlDb("database.db", "Memory")
 		self.TC = lambda n: " : ".join([f"{int(n/60):02d}", f"{int(n%60):02d}", "{:.2f}".format(round(n%60, 2))[-2:]])
 		self.event1 = None
-		self.timer: list = [300, 300]
+		self.timer: list = self.sql.get()
 		self.tcc:   list = [0, 0]
 		self.rt     = [[0, 0], 300]
 		self.switch = [0, 0, [0, [60, 1, 60, 1], 0]]
@@ -43,7 +59,6 @@ class main(App):
 		self.G1_1 = GridLayout(rows=2, size_hint=(1, .85))
 		self.G1_2 = GridLayout(cols=3, size_hint=(1, .15))
 		self.G1_1_1 = GridLayout(cols=2, size_hint=(1, .90))
-		
 		self.G1_1_2 = GridLayout(cols=3, size_hint=(1, .10))
 		self.b1_1_1 = B1(text="White", size_hint=(1, .825), on_press=lambda *args:self.b1_pressed(1), disabled=True)
 		self.b1_1_2 = B1(text="Black", size_hint=(1, .825), on_press=lambda *args:self.b1_pressed(0), disabled=True)
@@ -73,28 +88,17 @@ class main(App):
 		if self.rt[1] == 300:
 			self.b1_1_1.disabled = False
 		if self.b2_2.text == "play":
-			self.now = time.time()
-			print(self.rt)
-			self.rt = [[self.rt[0][0], self.rt[0][1]], None]
-			self.event1 = Clock.schedule_interval(self.update, .05)
-			self.b2_2.text = "pause"
+			self.now, self.rt, self.event1, self.b2_2.text, self.buttons[self.switch[0]].disabled = time.time(), [[self.rt[0][0], self.rt[0][1]], None], Clock.schedule_interval(self.update, .005), "pause", False
 			self.b_setting1(True, True)
-			self.buttons[self.switch[0]].disabled = False
 		elif self.b2_2.text == "pause":
 			self.b_setting1(False, False)
 			self.rt[0][not self.switch[0]] = (self.now - time.time())
 			self.tcc[self.switch[0]] += abs(self.rt[0][self.switch[0]])
 			Clock.unschedule(self.event1)
 			self.b_setting2(True, True)
-			self.b2_2.text = "play"
-			self.btn    = ["play", "restart"]
-			self.switch[1] = 0
+			self.b2_2.text, self.btn, self.switch[1] = "play", ["play", "restart"], 0
 		elif self.b2_2.text == "restart":
-			self.btn    = ["play", "restart", "setting"]
-			self.switch[1] = 1
-			self.tcc    = [0, 0]
-			self.rt     = [[0, 0], 300]
-			self.switch[0] = 0
+			self.btn, self.switch[1], self.tcc, self.rt, self.switch[0] = ["play", "restart", "setting"], 1, [0, 0], [[0, 0], 300], 0
 			self.b_setting2(True, True)
 			self.updates()
 		elif self.b2_2.text == "setting":
@@ -107,6 +111,7 @@ class main(App):
 				self.b2_2.text = "confirm"
 			self.switch[2][0] += 1
 		elif self.b2_2.text == "confirm":
+			self.sql.update(["data1", "data2"], self.timer)
 			self.switch[2][2], self.switch[2][0] = 0, 0
 			self.b_setting2(True, True)
 			self.b2_2.text, self.b2_1.text, self.b2_3.text = "setting", "<<", ">>"
@@ -122,6 +127,7 @@ class main(App):
 				if mv == 0: self.mode += -1
 				else: self.mode += 1
 				self.l1_1_3.text=str(self.mode)
+				self.tcc = [0, 0]
 			else:
 				if mv == 0:
 					self.timer[0 if self.switch[2][0]<2 else 1] += -self.switch[2][1][self.switch[2][0]]
@@ -144,10 +150,9 @@ class main(App):
 		else:
 			self.tcc[self.switch[0]] += abs(self.rt[0][self.switch[0]])
 			self.tcc[not self.switch[0]] += -self.mode
-			self.labels[not self.switch[0]].text = self.TC((self.timer[self.switch[0]]-self.tcc[not self.switch[0]])+self.rt[0][not self.switch[0]])
+			self.labels[not self.switch[0]].text = self.TC((self.timer[not self.switch[0]]-self.tcc[not self.switch[0]])+self.rt[0][not self.switch[0]])
 			self.rt[0][self.switch[0]], self.rt[1] = 0, self.switch[0]
-			Time = (self.timer[self.switch[0]]-self.tcc[self.switch[0]])+self.rt[0][self.switch[0]]
-			self.now = time.time()
+			Time, self.now = (self.timer[self.switch[0]]-self.tcc[self.switch[0]])+self.rt[0][self.switch[0]], time.time()
 		Time = (self.timer[self.switch[0]]-self.tcc[self.switch[0]])+self.rt[0][self.switch[0]]
 		self.labels[self.switch[0]].text = self.TC(Time)
 		if Time <= 0:
@@ -155,9 +160,7 @@ class main(App):
 				self.play_sound(1)
 			self.labels[self.switch[0]].text = self.TC(0)
 			self.b_setting2(True, True)
-			self.b2_2.text = "restart"
-			self.btn    = ["restart"]
-			self.switch[1] = 0
+			self.b2_2.text, self.btn, self.switch[1] = "restart", ["restart"], 0
 			self.b_setting1(False, False)
 			Clock.unschedule(self.event1)
 	def play_sound(self, *args):
